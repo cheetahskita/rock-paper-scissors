78097823b0f6253d13c3470b513db420c9567ce2
Stephen <stephen21.kita@gmail.com>
2020-04-13 11:55:56 -0400

    add game function and player selection helper functions

--- scripts.js
+++ scripts.js
@@ -1,34 +1,59 @@
-function computerPlay(){
-    let choice = Math.floor(3*Math.random());
-    switch(choice){
-        case 0:
-            choice = 'rock';
-            break;
-        case 1:
-            choice = 'paper';
-            break;
-        case 2:
-            choice = 'scissors';
-            break;
+game();
+
+function game(){
+
+    for (let i = 1; i<4; i++){
+        let playerSelection = playerPlay();
+        let computerSelection = computerPlay();
+
+        console.log(playRound(playerSelection,computerSelection,i));
     }
-    return choice;
-}
 
-let i = 0, j = 0, k = 0, l;
+    function playRound(playerSelection,computerSelection,numRound){
+        let playerChoice = playerSelection;
+        let computerChoice = computerSelection;
 
-for (l=0; l < 3000000; l++){
-    let choice = Math.floor(3*Math.random())
-    switch(choice){
-        case 0:
-            i++;
-            break;
-        case 1:
-            j++
-            break;
-        case 2:
-            k++;
-            break;
+        if (playerChoice == computerChoice){
+            winLoseMsg = 'You tied!';
+        }else if (playerChoice == 'rock' && computerChoice == 'scissors'){
+            winLoseMsg = 'You win!';
+        }else if (playerChoice == 'scissors' && computerChoice == 'paper'){
+            winLoseMsg = 'You win!';
+        }else if (playerChoice == 'paper' && computerChoice == 'rock'){
+            winLoseMsg = 'You win!';
+        }else{
+            winLoseMsg = 'You lose!';
+        }
+
+        let msg = `Round #${numRound}: You chose ${playerChoice} and the computer chose ${computerChoice}. ${winLoseMsg}`;
+        return msg;
     }
-}
 
-alert(`Rock count: ${i}. Paper count: ${j}. Scissors count: ${k}.`)
+    function computerPlay(){
+        let choice = Math.floor(3*Math.random());
+        switch(choice){
+            case 0:
+                choice = 'rock';
+                break;
+            case 1:
+                choice = 'paper';
+                break;
+            case 2:
+                choice = 'scissors';
+                break;
+        }
+        return choice;
+    }
+
+    function playerPlay(){
+        let playerChoice = prompt('Rock, paper, or scissors?');
+        playerChoice = playerChoice.toLowerCase();
+        if (playerChoice == 'rock' || playerChoice == 'paper' || playerChoice == 'scissors'){
+        } else{
+            playerChoice = prompt('Please only enter rock, paper, or scissors.');
+            playerChoice = playerChoice.toLowerCase();
+        }
+        return playerChoice;
+    }
+
+}
