74509873a587a2e51ee78c42fbc00d2f3b0746d2
Stephen <stephen21.kita@gmail.com>
2020-04-21 16:50:05 -0400

    add animation to each round

--- scripts.js
+++ scripts.js
@@ -1,5 +1,4 @@
-let roundNum = 0;
-
+let roundNum = 0, playerScore = 0, computerScore = 0;
 
 buttons = document.querySelectorAll('button');
 buttons.forEach( (button) => {
@@ -7,13 +6,26 @@ buttons.forEach( (button) => {
 });
 
 function playGame(button){
-    roundNum++
+    clearResults();
+    roundNum++;
+
     let playerSelection = button.id;
     let computerSelection = computerPlay();
 
-    msg = playRound(playerSelection,computerSelection, roundNum);
+    let msg = playRound(playerSelection,computerSelection, roundNum);
 
-    document.getElementById('results').innerText = msg;
+    displayResults(msg);
+}
+
+function clearResults(){
+    //let text = document.getElementById('text');
+    //text.forEach( => childnodes.innerText = "");
+
+    document.getElementById('round').innerText = '';
+    document.getElementById('intro').innerText = '';
+    document.getElementById('choice').innerText = '';
+    document.getElementById('results').innerText = '';
+    document.getElementById('score').innerText = '';
 }
 
 function playRound(playerSelection, computerSelection, roundNum){
@@ -24,19 +36,24 @@ function playRound(playerSelection, computerSelection, roundNum){
         winLoseMsg = 'You tied!';
     }else if (playerChoice == 'rock' && computerChoice == 'scissors'){
         winLoseMsg = 'You win!';
+        playerScore += 1;
     }else if (playerChoice == 'scissors' && computerChoice == 'paper'){
         winLoseMsg = 'You win!';
+        playerScore += 1;
     }else if (playerChoice == 'paper' && computerChoice == 'rock'){
         winLoseMsg = 'You win!';
+        playerScore += 1;
     }else{
         winLoseMsg = 'You lose!';
+        computerScore += 1;
     }
 
-    let msg = `Round ${roundNum}:
-    
-        You chose ${playerChoice} and the computer chose ${computerChoice}.
+    let msg = [''];
+    msg[0] = `Round: ${roundNum}`;
+    msg[1] = `You chose ${playerChoice} <> Computer chose ${computerChoice}`;
+    msg[2] = winLoseMsg;
+    msg[3] = `Score: You ${playerScore} - Computer ${computerScore}`;
 
-        ${winLoseMsg}`;
     return msg;
 }
 
@@ -56,13 +73,32 @@ function computerPlay(){
     return choice;
 }
 
-function playerPlay(){
-    let playerChoice = prompt('Rock, paper, or scissors?');
-    playerChoice = playerChoice.toLowerCase();
-    if (playerChoice == 'rock' || playerChoice == 'paper' || playerChoice == 'scissors'){
-    } else{
-        playerChoice = prompt('Please only enter rock, paper, or scissors.');
-        playerChoice = playerChoice.toLowerCase();
-    }
-    return playerChoice;
+async function displayResults(msg){
+    let timeDelay = 500;
+
+    document.getElementById('round').innerText = msg[0];
+    await animateGame(2*timeDelay);
+    document.getElementById('choice').innerText = msg[1];
+    await sleep(2*timeDelay);
+    document.getElementById('results').innerText = msg[2];
+    await sleep(timeDelay);
+    document.getElementById('score').innerText = msg[3];
 }
+
+async function animateGame(timeDelay){
+    let shootTime = 0.4*timeDelay;
+
+    await sleep(timeDelay);
+    document.getElementById('intro').innerText = 'Rock';
+    await sleep(shootTime);
+    document.getElementById('intro').innerText += ', Paper';
+    await sleep(shootTime);
+    document.getElementById('intro').innerText += ', Scissors';
+    await sleep(shootTime);
+    document.getElementById('intro').innerText += ', Shoot!';
+    await sleep(timeDelay);
+}
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
